dc55dcb81b7847fb08058fb3035084d7902f0fd5
RodrockSoftware <1corintios2.9@gmail.com>
2025-07-26 09:53:01 -0300

    Modificação inicial em MainActivity

--- app/src/main/java/com/example/spacemeta/MainActivity.kt
+++ app/src/main/java/com/example/spacemeta/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.spacemeta.screens.NavGraph
-import com.example.spacemeta.screens.PeriodoSelectionScreen
 import com.example.spacemeta.ui.theme.SpaceMetaTheme
 import com.google.firebase.FirebaseApp
 
